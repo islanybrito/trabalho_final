63a021d57589745e09a8ada034022a8071d39ec5
islany <islanybritobeserra@gmail.com>
2024-11-20 17:22:14 -0300

    trabalho_final

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,7 +2,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class Usuario(AbstractUser):
     nome = models.CharField(max_length=150)
     cpf = models.CharField(max_length=11, unique=True)
